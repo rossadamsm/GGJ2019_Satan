99ff97b71981fa1ebba0b75ef62a0b70e6098bb9
rossadamsm <rossadamsm@gmail.com>
2019-01-25 20:22:26 +0200

    Added running animation of satan

---
+++ SatanComesHome/Assets/Scripts/RossMovementController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class RossMovementController : MonoBehaviour
+{
+    public KeyCode walkKey = KeyCode.W;
+
+    public Animator myAnimator;
+
+    private void Awake()
+    {
+        myAnimator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(walkKey))
+        {
+            myAnimator.SetBool("walk", true);
+        }
+        else
+        {
+            myAnimator.SetBool("walk", false);
+        }
+    }
+}
