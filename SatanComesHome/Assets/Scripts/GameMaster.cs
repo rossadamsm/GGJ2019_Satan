1293b0eb25831a54ee5b4778d143fac96890c695
David Welman <David@BPDEV2000.NET>
2019-01-25 20:37:59 +0200

    Added game master and collectable script

---
+++ SatanComesHome/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+	private float timerStartValue = 60.0f;
+	private float timer = 0;
+	private int score = 0;
+
+    void Start()
+    {
+		Random.InitState((int)System.DateTime.Now.Ticks);
+
+		timer = timerStartValue;
+    }
+
+    void Update()
+    {
+		TickTimer();
+
+		//If timer ticks all the way down, end game
+
+		//Update UI
+
+		//Scoring
+			//If target is returned
+				//Assign new target
+    }
+
+	private void TickTimer()
+	{
+		timer -= Time.deltaTime;
+	}
+
+	private void GenerateLevel()
+	{
+
+	}
+
+	private void EndGame()
+	{
+
+	}
+
+	public void ChangeTimer(float delta)
+	{
+		timer += delta;
+	}
+}
