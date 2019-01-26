5a7db09cb1b7ffc7013e620824111b09fcf88f03
rossadamsm <rossadamsm@gmail.com>
2019-01-26 08:50:36 +0200

    messing around with virtual cameras

---
+++ SatanComesHome/Assets/Scripts/CameraController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private new Camera camera;
+
+    private void Awake()
+    {
+        camera = GetComponent<Camera>();
+    }
+}
