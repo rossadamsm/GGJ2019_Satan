ad7f1e083f584a62f3e0258c35e92800a5fa897c
rossadamsm <rossadamsm@gmail.com>
2019-01-26 17:48:03 +0200

    added soundmanager prefab

---
+++ SatanComesHome/Assets/Scripts/StoryManager.cs
@@ -0,0 +1,35 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+public class StoryManager : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera[] cinemachineVirtualCameras;
+
+    private int frameIndex = 0;
+
+    private int previousPriority;
+
+    private void Awake()
+    {
+        previousPriority = cinemachineVirtualCameras[0].Priority;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            MoveToNextFrame();
+        }
+    }
+
+    public void MoveToNextFrame()
+    {
+        frameIndex++;
+
+        if (frameIndex >= cinemachineVirtualCameras.Length)
+            frameIndex = 0;
+
+        previousPriority *= 2;
+        cinemachineVirtualCameras[frameIndex].Priority = previousPriority;
+    }
+}
