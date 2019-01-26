e0211302d848701f96742cc3f875e6a1fd08b93e
David Welman <David@BPDEV2000.NET>
2019-01-26 13:23:53 +0200

    Added tag to satan

--- SatanComesHome/Assets/Scripts/Portal.cs
+++ SatanComesHome/Assets/Scripts/Portal.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class Portal : MonoBehaviour
 {
-	// Start is called before the first frame update
-	void Start()
-	{
+	private Portal otherPortal;
 
-	}
-
-	// Update is called once per frame
-	void Update()
-	{
-
-	}
+	
 }
