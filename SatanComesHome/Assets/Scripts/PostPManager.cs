13a8e93687976ee9b393e2b13da1238debec5cc4
Reyno van der Westhuizen <reyno@BPDEV2000.NET>
2019-01-26 15:42:33 +0200

    profiles

---
+++ SatanComesHome/Assets/Scripts/PostPManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class PostPManager : MonoBehaviour
+{
+    public Camera view;
+    public PostProcessProfile earthprofile;
+    public PostProcessProfile hellprofile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        satan = FindObjectOfType<CharacterController>();
+        view = Camera.main;
+    }
+
+    public void gotoEarth()
+    {
+        PostProcessVolume effects = view.GetComponent<PostProcessVolume>();
+        effects.profile = earthprofile;
+    }
+
+    public void gotoHell()
+    {
+        PostProcessVolume effects = view.GetComponent<PostProcessVolume>();
+        effects.profile = hellprofile;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
