95db012180abf7c58abb1d9fe46fcd19408d1fd9
Reyno van der Westhuizen <reyno@BPDEV2000.NET>
2019-01-26 15:43:07 +0200

    bleh

--- SatanComesHome/Assets/Scripts/PostPManager.cs
+++ SatanComesHome/Assets/Scripts/PostPManager.cs
@@ -12,7 +12,6 @@ public class PostPManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        satan = FindObjectOfType<CharacterController>();
         view = Camera.main;
     }
 
