85eae912d5074d274446624083fa901ae095571f
rossadamsm <rossadamsm@gmail.com>
2019-01-26 11:31:40 +0200

    added spriteimporter settings

---
+++ SatanComesHome/Assets/Editor/SpriteImporter.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class SpriteImporter : AssetPostprocessor
+{
+    void OnPostprocessSprites(Texture2D texture, Sprite[] sprites)
+    {
+        TextureImporter importer = assetImporter as TextureImporter;
+
+        // only change sprite import settings on first import, so we can change those settings for individual files
+
+        string name = importer.assetPath.ToLower();
+        if (File.Exists(AssetDatabase.GetTextMetaFilePathFromAssetPath(name)))
+        {
+            return;
+        }
+
+        // adjust values for pixel art
+
+        importer.spritePixelsPerUnit = 1;
+        importer.mipmapEnabled = false;
+        importer.filterMode = FilterMode.Point;
+        importer.textureFormat = TextureImporterFormat.AutomaticTruecolor;
+        importer.compressionQuality = 0;
+        importer.textureCompression = TextureImporterCompression.Uncompressed;
+
+        // access the TextureImporterSettings to change the spriteAlignment value
+
+        TextureImporterSettings textureSettings = new TextureImporterSettings();
+        importer.ReadTextureSettings(textureSettings);
+
+        textureSettings.spritePivot = new Vector2(0.5f, 0f);
+        textureSettings.spriteAlignment = (int)SpriteAlignment.BottomCenter;
+
+        importer.SetTextureSettings(textureSettings);
+
+        importer.SaveAndReimport();
+
+        Debug.Log("Sprites: " + sprites[0].name);
+    }
+}
--- SatanComesHome/Assets/Scripts/PriestAI.cs
+++ SatanComesHome/Assets/Scripts/PriestAI.cs
@@ -1,5 +1,81 @@
 ﻿using UnityEngine;
 
+public enum PriestState
+{
+    Idle,
+    Roam,
+    Chase
+}
+
 public class PriestAI : MonoBehaviour
 {
+
+    public PriestState myState;
+    public float speed = 1;
+
+    private Animator animator;
+    private Rigidbody2D rb;
+    private Vector2 Move, roamDirection = new Vector2(1,0);
+    private Transform player;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
+        player = FindObjectOfType<CharacterController>().transform;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        roamDirection = collision.relativeVelocity;
+    }
+
+    private void Update()
+    {
+        switch (myState)
+        {
+            case PriestState.Idle:
+                Idle();
+                break;
+            case PriestState.Roam:
+                Roam();
+                break;
+            case PriestState.Chase:
+                Chase();
+                break;
+            default:
+                break;
+        }
+
+        if (Move.Equals(Vector2.zero))
+            animator.SetBool("walk", false);
+        animator.SetBool("walk", true);
+
+        if (Move.x < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+        else
+            transform.localScale = new Vector3(1, 1, 1);
+
+        rb.velocity = Move * speed;
+    }
+
+    private void Chase()
+    {
+        Vector2 direction = player.position - transform.position;
+        direction.Normalize();
+        Move = direction;
+    }
+
+    private void Idle()
+    {
+        Move = new Vector2(0, 0);
+    }
+
+    private void Roam()
+    {
+        //Move = new Vector2(UnityEngine.Random.Range(-1f,1f), Random.Range(-1f, 1f));
+        Move = roamDirection;
+        Move.Normalize();
+    }
 }
