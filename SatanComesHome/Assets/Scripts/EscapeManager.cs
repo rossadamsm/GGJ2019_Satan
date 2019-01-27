588b7acff61d241859e65a503b6ea6eafa8061ed
David Welman <David@BPDEV2000.NET>
2019-01-27 10:31:46 +0200

    Added escape manager

---
+++ SatanComesHome/Assets/Scripts/EscapeManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscapeManager : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			Application.Quit();
+		}
+    }
+}
